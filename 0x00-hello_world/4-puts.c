4853c4a82f3a37e4d124a6d5b8a23783c567fdf0
Dagmawi Amanuel <dagmawiamanuel@gmail.com>
2022-04-10 06:09:39 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints some output
+ * main - prints out a string to stdout
+ * Return: 0 is success
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
