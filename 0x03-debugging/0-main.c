a9b65757bc78c3cb566ed8f600d6f9c731786819
Dagmawi Amanuel <dagmawiamanuel@gmail.com>
2022-04-13 03:29:02 -0700

    main.c and main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check if the integer is positiv, negative or zero
+ * @n: the interger being checked
+ * return: Always 0
+ */
+int main(void)
+{
+	int n;
+	n = 0;
+	positive_or_negative(n);
+	return (0);
+}
