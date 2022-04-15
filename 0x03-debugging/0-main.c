f92c6091d33e2afffcc6848d69a252b30ee6a6f7
Dagi07 <56720713+Dagi07@users.noreply.github.com>
2022-04-15 22:55:47 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check if the integer is positiv, negative or zero
@@ -9,7 +8,9 @@
 int main(void)
 {
   int i;
+  
   i = 0;
   positive_or_negative(i);
+  
   return (0);
 }
