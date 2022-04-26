29513bcbb3edd224a6f36a8fe19d4c77f70fa772
Dagi07 <56720713+Dagi07@users.noreply.github.com>
2022-04-27 01:01:08 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,15 +5,16 @@
  * @s: A pointer to the memory area to be filled.
  * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
+ *
  * Return: A pointer to the filled memory area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+  unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-      s[i] = b;
-    }
-    return (s);
+  for (i = 0; i < n; i++)
+  {
+    s[i] = b;
+  }
+  return (s);
 }
